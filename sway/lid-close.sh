478e464271c670518a5924db47b2f6e5d06dafc6
albertovito <alberto.vito14@protonmail.com>
2023-02-23 10:32:18 +0100

    HDMI status for lid script

--- sway/lid-close.sh
+++ sway/lid-close.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
 if grep -q open /proc/acpi/button/lid/LID0/state; then
-    swaymsg output eDP-1 enable
+    swaymsg output eDP-1 enable 
 else
     swaymsg output eDP-1 disable
+fi
+
+HDMI_STATUS="$(cat /sys/class/drm/card0-HDMI-A-1/status)"
+
+if [ "${HDMI_STATUS}" = connected ]; then
+ swaymsg workspace number 4
+else
+ swaymsg workspace number 1
 fi
