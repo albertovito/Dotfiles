8d803969d6152119b83011c7d38d349c022deedf
Alberto Vito <alberto.vito14@protonmail.com>
2023-03-04 19:08:10 +0100

    Remove window-manager

--- scripts/lxsession.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 1
-# Terminate already running lxsession istance
-killall lxsession
-# Launch lxsession
-lxsession &
--- scripts/udiskie.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sleep 1
-# Terminate already running udiskie istance
-killall udiskie
-# Launch udiskie
-udiskie -A -t
